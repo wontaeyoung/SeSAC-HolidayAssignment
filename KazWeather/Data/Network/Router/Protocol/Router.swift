1bf48c929f6cf4f6eb3417a7b312c61d2cc81feb
Taeyoung Won <hexter1994@gmail.com>
2024-02-09 00:31:53 +0900

    [Feat] Router 프로토콜 정의, URLRequest 변환 메서드 구현

---
+++ KazWeather/Data/Network/Router/Protocol/Router.swift
@@ -0,0 +1,38 @@
+//
+//  Router.swift
+//  KazWeather
+//
+//  Created by 원태영 on 2/8/24.
+//
+
+import Foundation
+
+protocol Router {
+  
+  var method: HTTPMethod { get }
+  var baseURL: String { get }
+  var path: String { get }
+  var headers: HTTPHeaders { get }
+  var parameters: HTTPParameters { get }
+}
+
+extension Router {
+  
+  func asURLRequest() throws -> URLRequest {
+    guard var components = URLComponents(string: baseURL) else {
+      throw HTTPError.invalidURL
+    }
+    components.path = path
+    components.queryItems = parameters.current
+    
+    guard let url = components.url else {
+      throw HTTPError.invalidURL
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    request.allHTTPHeaderFields = headers.current
+    
+    return request
+  }
+}
