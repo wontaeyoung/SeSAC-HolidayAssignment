e48b4da44481d32286e9d3b6f7eedbfe753f0b19
Taeyoung Won <hexter1994@gmail.com>
2024-02-08 21:12:57 +0900

    [Feat] GCD 매니저 클래스 추가

---
+++ KazWeather/Utility/Manager/GCDManager.swift
@@ -0,0 +1,34 @@
+//
+//  GCDManager.swift
+//  KazWeather
+//
+//  Created by 원태영 on 2/8/24.
+//
+
+import Foundation
+
+final class GCD {
+  static func main(work: @escaping () -> Void) {
+    DispatchQueue.main.async {
+      work()
+    }
+  }
+  
+  static func global(work: @escaping () -> Void) {
+    DispatchQueue.global().async {
+      work()
+    }
+  }
+  
+  static func main(after time: Double, work: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + time) {
+      work()
+    }
+  }
+  
+  static func global(after time: Double, work: @escaping () -> Void) {
+    DispatchQueue.global().asyncAfter(deadline: .now() + time) {
+      work()
+    }
+  }
+}
