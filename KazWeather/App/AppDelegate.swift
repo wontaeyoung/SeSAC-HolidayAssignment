9756fa61a2e35168bc6b6d1ff2fbf7b06e0231f1
Taeyoung Won <hexter1994@gmail.com>
2024-02-08 19:53:59 +0900

    [Fix] Primary 컬러 네임 중복 경고로 네이밍 수정

--- KazWeather/App/AppDelegate.swift
+++ KazWeather/App/AppDelegate.swift
@@ -20,7 +20,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     UINavigationBar.appearance().standardAppearance = appearence
     UINavigationBar.appearance().compactAppearance = appearence
     UINavigationBar.appearance().scrollEdgeAppearance = appearence
-    UIWindow.appearance().tintColor = .primary
+    UIWindow.appearance().tintColor = .brand
   }
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -32,10 +32,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: UISceneSession Lifecycle
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    
   }
 }
 
