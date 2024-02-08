fa55bdc53e6cc8301b08cd1b7788772ca1a7e387
Taeyoung Won <hexter1994@gmail.com>
2024-02-09 02:08:31 +0900

    [Add] UIControllable 프로토콜 추가

---
+++ KazWeather/Presenter/Common/Protocol/UIControllable.swift
@@ -0,0 +1,16 @@
+//
+//  UIControllable.swift
+//  KazWeather
+//
+//  Created by 원태영 on 2/9/24.
+//
+
+import UIKit
+
+protocol SearchBarControllable: UISearchBarDelegate { }
+
+protocol TableControllable: UITableViewDelegate, UITableViewDataSource { }
+
+protocol CollectionControllable: UICollectionViewDelegate, UICollectionViewDataSource {
+  func makeCollectionLayout() -> UICollectionViewFlowLayout
+}
