67fa05e12206f194babd66bc28e387503a2eaa74
Taeyoung Won <hexter1994@gmail.com>
2024-02-09 17:48:15 +0900

    [Add] 데일리 날씨 API 케이스 추가

--- KazWeather/Data/Network/Router/WeatherRouter.swift
+++ KazWeather/Data/Network/Router/WeatherRouter.swift
@@ -9,8 +9,10 @@ enum WeatherRouter: Router {
   
   case currentCity(id: Int)
   case currentCoord(lat: Double, long: Double)
-  case forecastCity(id: Int)
-  case forecastCoord(lat: Double, long: Double)
+  case forecast3hCity(id: Int)
+  case forecast3hCoord(lat: Double, long: Double)
+  case forecastDailyCity(id: Int)
+  case forecastDailyCoord(lat: Double, long: Double)
   
   var method: HTTPMethod {
     return .get
@@ -25,8 +27,11 @@ enum WeatherRouter: Router {
       case .currentCity, .currentCoord:
         return "/current"
         
-      case .forecastCity, .forecastCoord:
+      case .forecast3hCity, .forecast3hCoord:
         return "/forecast"
+        
+      case .forecastDailyCity, .forecastDailyCoord:
+        return "/forecast/daily"
     }
   }
   
@@ -35,7 +40,9 @@ enum WeatherRouter: Router {
   }
   
   var parameters: HTTPParameters {
-    var parameters = HTTPParameters().parameter(key: Parameter.appid.key, value: APIKey.openWeather)
+    var parameters = HTTPParameters()
+      .parameter(key: Parameter.appid.key, value: APIKey.openWeather)
+      .parameter(key: Parameter.lang.key, value: "kr")
     
     switch self {
       case let .currentCity(id):
@@ -47,14 +54,25 @@ enum WeatherRouter: Router {
           .parameter(key: Parameter.lat.key, value: lat.description)
           .parameter(key: Parameter.lon.key, value: long.description)
         
-      case let .forecastCity(id):
+      case let .forecast3hCity(id):
         return parameters
           .parameter(key: Parameter.id.key, value: id.description)
         
-      case let .forecastCoord(lat, long):
+      case let .forecast3hCoord(lat, long):
         return parameters
           .parameter(key: Parameter.lat.key, value: lat.description)
           .parameter(key: Parameter.lon.key, value: long.description)
+        
+      case let .forecastDailyCity(id):
+        return parameters
+          .parameter(key: Parameter.id.key, value: id.description)
+          .parameter(key: Parameter.cnt.key, value: 5.description)
+        
+      case let .forecastDailyCoord(lat, long):
+        return parameters
+          .parameter(key: Parameter.lat.key, value: lat.description)
+          .parameter(key: Parameter.lon.key, value: long.description)
+          .parameter(key: Parameter.cnt.key, value: 5.description)
     }
   }
 }
@@ -66,6 +84,8 @@ extension WeatherRouter {
     case id
     case lat
     case lon
+    case lang
+    case cnt
     
     var key: String {
       return self.rawValue
