c4212dac4878f27a851ae2fe1b1047ff54f5f6cd
Taeyoung Won <hexter1994@gmail.com>
2024-02-12 02:05:55 +0900

    [Feat] 초기값 생성 프로토콜 구현

---
+++ KazWeather/Data/Model/Protocol/DefaultValueProvidable.swift
@@ -0,0 +1,40 @@
+//
+//  DefaultValueProvidable.swift
+//  KazWeather
+//
+//  Created by 원태영 on 2/12/24.
+//
+
+protocol DefaultValueProvidable: Decodable {
+  static var defaultValue: Self { get }
+}
+
+extension String: DefaultValueProvidable {
+  static var defaultValue: String {
+    return "-"
+  }
+}
+
+extension Double: DefaultValueProvidable {
+  static var defaultValue: Double {
+    return 0.0
+  }
+}
+
+extension Float: DefaultValueProvidable {
+  static var defaultValue: Float {
+    return 0.0
+  }
+}
+
+extension Bool: DefaultValueProvidable {
+  static var defaultValue: Bool {
+    return false
+  }
+}
+
+extension Int: DefaultValueProvidable {
+  static var defaultValue: Int {
+    return 0
+  }
+}
