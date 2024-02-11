1305792e4fa64a264bed1d63b059b38d11c2ada3
Taeyoung Won <hexter1994@gmail.com>
2024-02-11 20:32:35 +0900

    [Feat] Icon과 Label이 있는 커스텀 뷰 구현

---
+++ KazDesignSystem/ReusableView/UIView/IconLabel.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+public final class IconLabel: BaseView {
+  
+  private let imageView = UIImageView()
+  private let label = UILabel()
+  
+  init(symbol: String?, text: String?) {
+    self.imageView.image = UIImage(systemName: symbol ?? "")
+    self.label.text = text
+    
+    super.init(frame: .zero)
+  }
+  
+  public override func setHierarchy() {
+    self.addSubview(imageView)
+    self.addSubview(label)
+  }
+  
+  public override func setAttribute() {
+    self.backgroundColor = .clear
+    imageView.contentMode = .scaleAspectFit
+  }
+  
+  override public func setConstraint() {
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    label.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      imageView.topAnchor.constraint(equalTo: self.topAnchor),
+      imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+      imageView.widthAnchor.constraint(equalToConstant: 22),
+      imageView.heightAnchor.constraint(equalToConstant: 22)
+    ])
+    
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: self.topAnchor),
+      label.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+      label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
+      label.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+    ])
+  }
+}
