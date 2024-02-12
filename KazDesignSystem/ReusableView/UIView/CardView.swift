6a629f30f51412ae84aefbf7fea6371bbef229a0
Taeyoung Won <hexter1994@gmail.com>
2024-02-12 17:01:19 +0900

    [Style] 카드 스타일 커스텀 UIView 구현

--- KazDesignSystem/ReusableView/UIView/CardView.swift
+++ KazDesignSystem/ReusableView/UIView/CardView.swift
@@ -9,3 +9,45 @@ public final class CardView: BaseView {
     self.layer.opacity = 0.4
   }
 }
+
+class ViewController: BaseViewController {
+  
+  let backgroundImageView = UIImageView()
+  let divider = Divider()
+  let cardView = CardView()
+  let imageLabel = IconLabel(
+    symbol: "calendar",
+    text: "3시간 간격의 일기예보"
+  )
+  
+  
+  override func setAttribute() {
+    view.backgroundColor = .white
+    view.addSubview(backgroundImageView)
+    view.addSubview(cardView)
+    view.addSubview(divider)
+    view.addSubview(imageLabel)
+    
+    backgroundImageView.image = .clouds
+    
+    backgroundImageView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+    
+    cardView.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+      make.height.equalTo(200)
+      make.horizontalEdges.equalToSuperview()
+    }
+    
+    imageLabel.snp.makeConstraints { make in
+      make.top.equalTo(cardView).offset(8)
+      make.horizontalEdges.equalTo(cardView).inset(8)
+    }
+    
+    divider.snp.makeConstraints { make in
+      make.top.equalTo(imageLabel.snp.bottom).offset(4)
+      make.horizontalEdges.equalTo(cardView)
+    }
+  }
+}
