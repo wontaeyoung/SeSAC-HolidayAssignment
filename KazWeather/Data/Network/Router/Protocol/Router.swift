3d989c4c3df47be2899b0c8a5cb751997af20839
Taeyoung Won <hexter1994@gmail.com>
2024-02-12 16:59:59 +0900

    [Fix] URL 생성 시 path 부분을 덮어쓰는 문제 해결

--- KazWeather/Data/Network/Router/Protocol/Router.swift
+++ KazWeather/Data/Network/Router/Protocol/Router.swift
@@ -18,9 +18,12 @@ protocol Router {
 
 extension Router {
   
+  /// URL 생성 실패 오류 발생
+  /// baseURL에 도메인 + 공통 path까지 포함시켜놓았기 때문
+  /// URLComponents는 도메인 주소 이후에 오는 /부터는 path로 간주하기 때문에, 덮어쓰기를 되어버림
+  /// 문제를 해결하면 Components 초기화 시 baseURL과 path를 합친 상태로 주입해야 함
   func asURLRequest() throws -> URLRequest {
-    guard var components = URLComponents(string: baseURL) else { throw HTTPError.invalidURL }
-    components.path = path
+    guard var components = URLComponents(string: baseURL + path) else { throw HTTPError.invalidURL }
     components.queryItems = parameters.current
     
     guard let url = components.url else { throw HTTPError.invalidURL }
