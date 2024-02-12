9090522f0e5dfb032c07fb5db49bc763aa07b952
Taeyoung Won <hexter1994@gmail.com>
2024-02-13 01:04:52 +0900

    [Fix] 카드뷰 배경색 투명도가 백그라운드에만 적용되도록 수정

--- KazDesignSystem/ReusableView/UIView/CardView.swift
+++ KazDesignSystem/ReusableView/UIView/CardView.swift
@@ -6,7 +6,7 @@ public final class CardView: BaseView {
   // MARK: - Life Cycle
   public override func setAttribute() {
     self.layer.cornerRadius = 15
-    self.layer.opacity = 0.4
+    self.backgroundColor = .weatherBackground.withAlphaComponent(0.4)
   }
 }
 
