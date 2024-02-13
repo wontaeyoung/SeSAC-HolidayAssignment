9914005e2f7420095bede6a739f0e8167b4e617c
Taeyoung Won <hexter1994@gmail.com>
2024-02-13 10:17:07 +0900

    [Clean] 데이터 응답 실패 사용자 안내 문구 수정

--- KazWeather/Data/Network/Basement/HTTPError.swift
+++ KazWeather/Data/Network/Basement/HTTPError.swift
@@ -45,7 +45,7 @@ enum HTTPError: AppError {
         return "요청에 실패했어요. 와이파이 연결을 확인하거나 데이터 네트워크 상태를 확인하시고, 잠시 후 다시 시도해주세요."
         
       case .noData, .invalidResponse, .unexceptedResponse, .dataDecodingFailed:
-        return "데이터 응답에 실패했어요. 잠시 후 다시 시도해주세요."
+        return "데이터 응답에 실패했어요. 잠시 후 다시 시도해주세요. 문제가 계속되면 개발자에게 문의해주세요."
     }
   }
 }
