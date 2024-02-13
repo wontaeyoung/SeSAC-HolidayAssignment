7fad3e1ed1cf0bd0dcb1f1f3a36f45e378a60b1e
Taeyoung Won <hexter1994@gmail.com>
2024-02-14 03:02:07 +0900

    [Feat] Table, Collection Delegate 구현, 추가 가공 데이터 바인딩

--- KazWeather/Presenter/Main/MainViewController.swift
+++ KazWeather/Presenter/Main/MainViewController.swift
@@ -38,6 +38,8 @@ final class MainViewController: BaseViewController, ViewModelController {
   // MARK: - Life Cycle
   override func loadView() {
     self.view = mainView
+    
+    mainView.setDelegate(with: self)
   }
   
   override func setHierarchy() {
@@ -54,10 +56,15 @@ final class MainViewController: BaseViewController, ViewModelController {
   override func bind() {
     viewModel.cityWeather.subscribe { cityWeather in
       self.mainView.updateCurrentWeatherUI(with: cityWeather)
+      self.mainView.reloadCollectionData()
     }
     
-    viewModel.forecastBy3H.subscribe { forecastBy3H in
+    viewModel.forecastBy3H3D.subscribe { forecastBy3H in
       self.mainView.updateForecastWeatherUI(with: forecastBy3H)
+    }
+    
+    viewModel.forecastBy5D.subscribe { forecastBy5D in
+      self.mainView.reloadTableData()
     }
   }
   
@@ -68,6 +75,85 @@ final class MainViewController: BaseViewController, ViewModelController {
   }
   
   @objc private func cityItemTapped() {
+    mainView.reloadTableData()
+  }
+}
+
+extension MainViewController: TableControllable {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.forecastBy5D.current.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: DailyForecastTableViewCell.identifier,
+      for: indexPath
+    ) as! DailyForecastTableViewCell
     
+    let data: CityWeather = viewModel.forecastBy5D.current[indexPath.row]
+    cell.updateUI(with: data, row: indexPath.row)
+    cell.selectionStyle = .none
+    
+    return cell
+  }
+}
+
+extension MainViewController: CollectionControllable {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return ExtraInfo.allCases.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: ExtraInfoCollectionViewCell.identifier,
+      for: indexPath
+    ) as! ExtraInfoCollectionViewCell
+    
+    let data = viewModel.cityWeather.current
+    let info = ExtraInfo.allCases[indexPath.row]
+    cell.updateUI(with: data, info: info)
+    
+    return cell
+  }
+}
+
+enum ExtraInfo: CaseIterable {
+  case wind
+  case cloud
+  case pressure
+  case humidity
+  
+  var title: String {
+    switch self {
+      case .wind:
+        return "바람 속도"
+        
+      case .cloud:
+        return "구름"
+        
+      case .pressure:
+        return "기압"
+        
+      case .humidity:
+        return "습도"
+    }
+  }
+  
+  var iconSymbol: String {
+    switch self {
+      case .wind:
+        return "wind"
+        
+      case .cloud:
+        return "cloud.fill"
+        
+      case .pressure:
+        return "water.waves.and.arrow.down"
+        
+      case .humidity:
+        return "humidity.fill"
+    }
   }
 }
